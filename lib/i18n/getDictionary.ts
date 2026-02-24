57bc4fc398e11d9d36378af7cf4978d5a5fe919f
Alboran Villa <alboranvilla@MacBook-Air-de-Alboran.local>
2026-02-24 15:02:29 +0800

    Fix dictionary typing for multi-language support

--- lib/i18n/getDictionary.ts
+++ lib/i18n/getDictionary.ts
@@ -1,15 +1,17 @@
 import { Locale } from "./config";
 
-export async function getDictionary(locale: Locale) {
+export type Dictionary = typeof import("./dictionaries/en.json");
+
+export async function getDictionary(locale: Locale): Promise<Dictionary> {
     switch (locale) {
         case "es":
-            return (await import("./dictionaries/es.json")).default;
+            return (await import("./dictionaries/es.json")).default as Dictionary;
         case "fr":
-            return (await import("./dictionaries/fr.json")).default;
+            return (await import("./dictionaries/fr.json")).default as Dictionary;
         case "de":
-            return (await import("./dictionaries/de.json")).default;
+            return (await import("./dictionaries/de.json")).default as Dictionary;
         case "en":
         default:
-            return (await import("./dictionaries/en.json")).default;
+            return (await import("./dictionaries/en.json")).default as Dictionary;
     }
 }
